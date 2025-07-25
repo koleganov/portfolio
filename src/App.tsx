54f926bb301a9ab5b1cfb15d411006dd877486cb
Koleganov Kostya <taggz1030@gmail.com>
2025-07-25 15:40:40 +0500

    about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
